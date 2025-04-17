e98356704346c26d560340e7d2181c53fb56f980
Mathis <51701372+nitreb@users.noreply.github.com>
2025-04-17 10:06:08 +0200

    Add dummy scores and colors to cell to compare difference (#62)

--- website/prepare.py
+++ website/prepare.py
@@ -9,7 +9,7 @@ def find_result_html(model: str) -> str:
     input_notebook = open(f"../assets/{model}_sample.report.ipynb")
     raw_notebook = json.load(input_notebook)
     for cell in raw_notebook["cells"]:
-        if "oceanbench.metrics.rmse_to_glorys(challenger_datasets)" in cell["source"]:
+        if "oceanbench.metrics.rmse_compared_to_glorys_variables(challenger_datasets)" in cell["source"]:
             html_output = cell["outputs"][0]["data"]["text/html"]
             cleaned_html_output = "".join([line.removesuffix("\n") for line in html_output])
             return cleaned_html_output
@@ -19,11 +19,11 @@ Path("_result_tables").mkdir(parents=True, exist_ok=True)
 for model in models:
     scores = {}
     result = find_result_html(model)
-    soup = BeautifulSoup(result)
+    soup = BeautifulSoup(result, features="html.parser")
     tbody = soup.find("tbody")
     rows = tbody.find_all("tr")
     for row in rows:
         variable = row.find("th").string
-        scores[variable] = {k: v.string for k, v in enumerate(row.find_all("td"))}
+        scores[variable] = {(k + 1): float(v.string) for k, v in enumerate(row.find_all("td"))}
     output = open(f"_result_tables/{model}.json", "w+")
     json.dump(scores, output)
