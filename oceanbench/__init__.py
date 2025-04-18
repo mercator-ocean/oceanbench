cacd0029ee4b070d41c7f5189deb323a86d94f73
Quentin Gaudel <qgaudel@mercator-ocean.fr>
2025-04-18 15:50:53 +0200

    82 cleanup all unused functions

--- oceanbench/__init__.py
+++ oceanbench/__init__.py
@@ -1,13 +1,9 @@
 from . import metrics
-from . import derived_quantities
-from . import plot
 from .evaluate import generate_notebook_to_evaluate
 from .version import __version__
 
 __all__ = [
     "metrics",
-    "derived_quantities",
-    "plot",
     "generate_notebook_to_evaluate",
     "__version__",
 ]
--- oceanbench/core/plot/geo_core.py
+++
@@ -1,7 +0,0 @@
-from matplotlib import pyplot
-import xarray
-
-
-def plot_geo_core(dataset: xarray.Dataset):
-    dataset.u_geo.plot(vmin=-1.5, vmax=1.5)
-    pyplot.show()
