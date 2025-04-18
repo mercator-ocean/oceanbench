6e2794be0f3e878cb11fe024dbfc31531ce33d3c
Quentin Gaudel <qgaudel@mercator-ocean.fr>
2025-04-18 17:05:59 +0200

    91 Evaluate XiHe

---
+++ challenger_datasets/xihe.py
@@ -0,0 +1,23 @@
+# Open XIHE forecasts with xarray
+import xarray
+from typing import List
+from datetime import datetime, timedelta
+
+
+def generate_dates(start_date_str, end_date_str, delta_days):
+    start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
+    end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
+    return [
+        (start_date + timedelta(days=i * delta_days)).strftime("%Y-%m-%d")
+        for i in range((end_date - start_date).days // delta_days + 1)
+    ]
+
+
+def _open_dataset(date_string: str) -> xarray.Dataset:
+    return xarray.open_dataset(
+        f"https://minio.dive.edito.eu/project-glonet/public/glonet_reforecast_2024/{date_string}.zarr",
+        engine="zarr",
+    )
+
+
+challenger_datasets: List[xarray.Dataset] = list(map(_open_dataset, generate_dates("2024-01-03", "2024-07-10", 7)))[:1]
