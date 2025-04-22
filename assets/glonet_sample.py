2a5007a6f9c2137c3c91de923b4b1e64f595c5ef
qgau <45388569+qgau@users.noreply.github.com>
2025-04-22 10:54:36 +0200

    90 update glonet sample with new glonet reforecast (#94)
    Co-authored-by: Quentin Gaudel <qgaudel@mercator-ocean.fr>

--- assets/glonet_sample.py
+++ assets/glonet_sample.py
@@ -1,95 +1,10 @@
 # Open GLONET forecast sample with xarray
-from functools import reduce
 import xarray
 from typing import List
 
-LATITUDE_CLIMATE_FORECAST_ATTRIBUTES = {
-    "standard_name": "latitude",
-    "long_name": "Latitude",
-    "units": "degrees_north",
-    "units_long": "Degrees North",
-    "axis": "Y",
-}
-
-LONGITUDE_CLIMATE_FORECAST_ATTRIBUTES = {
-    "standard_name": "longitude",
-    "long_name": "Longitude",
-    "units": "degrees_east",
-    "units_long": "Degrees East",
-    "axis": "X",
-}
-
-DEPTH_CLIMATE_FORECAST_ATTRIBUTES = {
-    "standard_name": "depth",
-    "long_name": "Depth",
-    "units": "m",
-    "units_long": "Meters",
-}
-
-TIME_CLIMATE_FORECAST_ATTRIBUTES = {
-    "standard_name": "time",
-    "long_name": "Time",
-    "axis": "T",
-}
-
-TEMPERATURE_CLIMATE_FORECAST_ATTRIBUTES = {
-    "standard_name": "sea_water_potential_temperature",
-}
-
-SALINITY_CLIMATE_FORECAST_ATTRIBUTES = {
-    "standard_name": "sea_water_salinity",
-}
-
-HEIGHT_CLIMATE_FORECAST_ATTRIBUTES = {
-    "standard_name": "sea_surface_height_above_geoid",
-}
-
-NORTHWARD_VELOCITY_CLIMATE_FORECAST_ATTRIBUTES = {
-    "standard_name": "northward_sea_water_velocity",
-}
-
-EASTWARD_VELOCITY_CLIMATE_FORECAST_ATTRIBUTES = {
-    "standard_name": "eastward_sea_water_velocity",
-}
-
-
-def _update_variable_attributes(
-    dataset: xarray.Dataset,
-    variable_name_and_attributes: tuple[str, dict[str, str]],
-) -> xarray.Dataset:
-    variable_name, attributes = variable_name_and_attributes
-    dataset[variable_name].attrs = attributes
-    return dataset
-
-
-def _add_climate_forecast_attributes(
-    dataset: xarray.Dataset,
-) -> xarray.Dataset:
-    return reduce(
-        _update_variable_attributes,
-        zip(
-            ["lat", "lon", "depth", "time", "thetao", "so", "zos", "vo", "uo"],
-            [
-                LATITUDE_CLIMATE_FORECAST_ATTRIBUTES,
-                LONGITUDE_CLIMATE_FORECAST_ATTRIBUTES,
-                DEPTH_CLIMATE_FORECAST_ATTRIBUTES,
-                TIME_CLIMATE_FORECAST_ATTRIBUTES,
-                TEMPERATURE_CLIMATE_FORECAST_ATTRIBUTES,
-                SALINITY_CLIMATE_FORECAST_ATTRIBUTES,
-                HEIGHT_CLIMATE_FORECAST_ATTRIBUTES,
-                NORTHWARD_VELOCITY_CLIMATE_FORECAST_ATTRIBUTES,
-                EASTWARD_VELOCITY_CLIMATE_FORECAST_ATTRIBUTES,
-            ],
-        ),
-        dataset,
-    )
-
-
 challenger_datasets: List[xarray.Dataset] = [
-    _add_climate_forecast_attributes(
-        xarray.open_dataset(
-            "https://minio.dive.edito.eu/project-glonet/public/glonet_reforecast_2024/2024-01-03.zarr",
-            engine="zarr",
-        )
+    xarray.open_dataset(
+        "https://minio.dive.edito.eu/project-glonet/public/glonet_full_2024/20240103.zarr",
+        engine="zarr",
     )
 ]
