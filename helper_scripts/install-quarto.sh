3f9c967b2839336a3f539c0058cb3091ef49d104
qgau <45388569+qgau@users.noreply.github.com>
2025-06-19 16:57:30 +0200

    139 Add tooling for website local development (#140)
    Co-authored-by: Quentin Gaudel <qgaudel@mercator-ocean.fr>

---
+++ helper_scripts/install-quarto.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+# SPDX-FileCopyrightText: 2025 Mercator Ocean International <https://www.mercator-ocean.eu/>
+#
+# SPDX-License-Identifier: EUPL-1.2
+
+SCRIPT_DIR=$( dirname $(readlink -f "${BASH_SOURCE[0]}") )
+pushd $SCRIPT_DIR > /dev/null
+
+which quarto
+returnValue=$?
+if [ $returnValue -ne 0 ]; then
+    curl -L https://github.com/quarto-dev/quarto-cli/releases/download/v1.7.23/quarto-1.7.23-linux-amd64.deb --output /tmp/quarto.deb
+    dpkg -i /tmp/quarto.deb
+fi
+
+popd > /dev/null
