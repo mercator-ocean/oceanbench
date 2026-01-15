2d32303b44f229eba26ffb6f855eb8eac9f7adaa
Zakaria Aissaabdi <zaissa@mercator-ocean.fr>
2026-01-15 10:56:44 +0100

    Testing

--- oceanbench/core/interpolate.py
+++ oceanbench/core/interpolate.py
@@ -10,7 +10,6 @@ def interpolate_1deg(data: xr.Dataset) -> xr.Dataset:
     # create target lat/lon values at 1 deg
     new_lat = np.arange(np.floor(data.lat.min()), np.ceil(data.lat.max()), 1)
     new_lon = np.arange(np.floor(data.lon.min()), np.ceil(data.lon.max()), 1)
-    print("interpolating")
     # interpolate
     if hasattr(data, "time") and hasattr(data, "depth"):
         data = data.chunk({"time": 1, "lat": -1, "lon": -1, "depth": 1})
