95be7d5a4dd44a49153f42be0a44756bd705044b
ZakariaAissaabdi <zaissa@mercator-ocean.fr>
2026-02-06 16:43:19 +0100

    171 evaluate 1 degree models (#178)
    Signed-off-by: Julien Seillade <jseillade@mercator-ocean.fr>

--- assets/glonet_1_degree_sample.py
+++ assets/glonet_1_degree_sample.py
@@ -1,10 +1,12 @@
-# Open WENHAI forecast sample with xarray
+# Open GLONET forecast sample with xarray
 from datetime import datetime
 import xarray
+from oceanbench.core.interpolate import interpolate_1_degree
 
 challenger_dataset: xarray.Dataset = xarray.open_mfdataset(
     [
-        "https://minio.dive.edito.eu/project-oceanbench/public/WENHAI/20240103.zarr",
+        "https://minio.dive.edito.eu/project-oceanbench/public/glonet_full_2024/20240103.zarr",
+        "https://minio.dive.edito.eu/project-oceanbench/public/glonet_full_2024/20240110.zarr",
     ],
     engine="zarr",
     preprocess=lambda dataset: dataset.rename({"time": "lead_day_index"}).assign({"lead_day_index": range(10)}),
@@ -15,7 +17,9 @@ challenger_dataset: xarray.Dataset = xarray.open_mfdataset(
     {
         "first_day_datetime": [
             datetime.fromisoformat("2024-01-03"),
+            datetime.fromisoformat("2024-01-10"),
         ]
     }
 )
+challenger_dataset = interpolate_1_degree(challenger_dataset)
 challenger_dataset
