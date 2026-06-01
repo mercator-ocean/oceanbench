c9169a27fd54c4f485f57c59c6aa2cd27bc3af1a
Julien <jseillade@mercator-ocean.fr>
2026-06-01 11:44:03 +0200

    Sample Lagrangian particles with area weights
    Signed-off-by: Julien <jseillade@mercator-ocean.fr>

--- oceanbench/core/lagrangian_trajectory.py
+++ oceanbench/core/lagrangian_trajectory.py
@@ -330,10 +330,16 @@ def _get_random_ocean_points_from_file(
 
     n = min(n, n_available_ocean_points)
 
+    sampling_probabilities = _ocean_point_area_probabilities(latitude_values)
     numpy.random.seed(seed)
-    idx = numpy.random.choice(len(latitude_values), n, replace=False)
+    idx = numpy.random.choice(len(latitude_values), n, replace=False, p=sampling_probabilities)
 
     return latitude_values[idx], longitude_values[idx]
+
+
+def _ocean_point_area_probabilities(latitude_values: numpy.ndarray) -> numpy.ndarray:
+    point_weights = numpy.cos(numpy.deg2rad(numpy.asarray(latitude_values, dtype=float)))
+    return point_weights / point_weights.sum()
 
 
 def _surface_ocean_point_mask(
