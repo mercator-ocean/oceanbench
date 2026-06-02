1968e779c364171b513100d570c83f2abf67d644
Julien <jseillade@mercator-ocean.fr>
2026-06-02 09:30:39 +0200

    Add GLONET2 e230 report metadata

--- website/helpers/challenger_metadata.py
+++ website/helpers/challenger_metadata.py
@@ -103,6 +103,16 @@ CHALLENGERS = {
         "initial_conditions": "GLO12/IFS",
         "resolution": "1/12\u00b0",
     },
+    "glonet2_e230_icefix_r10": {
+        "label": "GLONET2 e230 icefix r10",
+        "url": "https://glonet.lab.dive.edito.eu",
+        "organisation": "Mercator Ocean",
+        "organisation_url": "https://mercator-ocean.eu",
+        "method": "ML-based",
+        "forecast_type": "Deterministic",
+        "initial_conditions": "GLO12/IFS",
+        "resolution": "1/12\u00b0",
+    },
     "langya": {
         "label": "LangYa",
         "url": "https://arxiv.org/abs/2412.18097",
