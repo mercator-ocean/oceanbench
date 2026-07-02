fd1b5e986cc228e71e2384fe61769470d4d776af
ccros <ccros@mercator-ocean.fr>
2026-07-02 11:35:19 +0200

    minor change to use Static pages process on EDITO

--- website/build-website.sh
+++ website/build-website.sh
@@ -9,17 +9,23 @@ set -euo pipefail
 SCRIPT_DIR=$( dirname $(readlink -f "${BASH_SOURCE[0]}") )
 pushd "$SCRIPT_DIR" > /dev/null
 
+# install system build tools (needed for pandas, matplotlib, etc.)
+apt-get update && apt-get install -y build-essential gcc g++ make
+
 if ! command -v quarto > /dev/null; then
-    curl -L https://github.com/quarto-dev/quarto-cli/releases/download/v1.7.23/quarto-1.7.23-linux-amd64.deb --output /tmp/quarto.deb
-    dpkg -i /tmp/quarto.deb
+  curl -L https://github.com/quarto-dev/quarto-cli/releases/download/v1.7.23/quarto-1.7.23-linux-amd64.deb --output /tmp/quarto.deb
+  dpkg -i /tmp/quarto.deb
 fi
 
 rm -rf reports _site
 
 pip install -r requirements.txt
+
 quarto render --to html
 
 mkdir -p /app/repository
 cp -r _site/* /app/repository
 
 popd > /dev/null
+
+
